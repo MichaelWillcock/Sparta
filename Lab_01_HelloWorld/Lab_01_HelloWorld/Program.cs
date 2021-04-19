3533bcee515a6b02767850cffc7019195d71125b
Michael Willcock <michaelwillcock7@gmail.com>
2021-04-19 16:33:04 +0100

    Initial Commit

---
+++ Lab_01_HelloWorld/Lab_01_HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lab_01_HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int x = 100;
+            x += 10;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+                    x += i;
+            }
+        }
+    }
+}
