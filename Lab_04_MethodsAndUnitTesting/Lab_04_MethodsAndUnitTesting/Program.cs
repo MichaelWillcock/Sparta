29d061b8d0e7249194418ef8a2c43344bc9f2f89
Michael Willcock <michaelwillcock7@gmail.com>
2021-04-20 13:34:41 +0100

    Commit example methods and initial calculator structure

--- Lab_04_MethodsAndUnitTesting/Lab_04_MethodsAndUnitTesting/Program.cs
+++ Lab_04_MethodsAndUnitTesting/Lab_04_MethodsAndUnitTesting/Program.cs
@@ -2,11 +2,52 @@
 
 namespace Lab_04_MethodsAndUnitTesting
 {
-    class Program
+    public class Methods
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            //DoThis1();
+
+            //var output = DoThis2();
+
+            //var output = DoThis3(10, "Sparta Global");
+
+            var output = DoThat(10);
         }
+        public static void DoThis1()
+        {
+            Console.WriteLine("I am doing something");
+        }
+        public static int DoThis2()
+        {
+            Console.WriteLine("I am returning 100");
+            return 100;
+        }
+        public static int DoThis3(int x, string y)
+        {
+            Console.WriteLine(y);
+            return x * x;
+        }
+        public static int DoThat(int x, string y = "Default")
+        {
+            Console.WriteLine(y);
+            return x * x;
+        }
+        //static int Add(int number1, int number2)
+        //{
+        //    return number1 + number2;
+        //}
+        //static int Subtract(int number1, int number2)
+        //{
+        //    return number1 - number2;
+        //}
+        //static int Multiply(int number1, int number2)
+        //{
+        //    return number1 * number2;
+        //}
+        //static double Add(double number1, double number2)
+        //{
+        //    return number1 / number2;
+        //}
     }
 }
