beddab6d14a9876c74b8ac7e0eddbb2312c245aa
Michael Willcock <michaelwillcock7@gmail.com>
2021-04-20 13:13:58 +0100

    Initial commit

---
+++ Lab_02_variabletypes/Lab_02_variabletypes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lab_02_variabletypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //integer overflow and underflow
+            //checked
+            //{
+            //    int a = Int32.MaxValue;
+            //    int b = a + 1;
+            //    Console.WriteLine(a);
+            //    Console.WriteLine(b);
+            //}
+            //var x = (1, "michael");
+            //var y = new List<int>{1, 2, 3};
+
+        }
+    }
+}
---
+++ Lab_03_Operators/Lab_03_Operators/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Lab_03_Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var sum = 3 + 1;
+            //var years = 11;
+            //var dogYears = years * 7;
+            //var pounds = 15;
+            //var kilos = pounds / 2.2;
+            //var average = (2 + 5 + 1 + 8) / 4;
+            //var a = 0.5 / 0.2;
+            //var b = 5 / 2;
+            //var c = 5.0 / 2;
+            //var days = 26;
+            //Console.WriteLine("26 days is " + (days / 7) + " weeks and " + (days % 7) + " days");
+            //bool isWearingAParachute = false;
+            //bool jumpOutOfPlane = true;
+
+            //if (isWearingAParachute == true & jumpOutOfPlane == true)
+            //{
+            //    Console.WriteLine("Jump");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Don't Jump");
+            //}
+            //Console.WriteLine(5 & 1);
+            //Console.WriteLine(5 | 1);
+            //Console.WriteLine(9 << 3);
+            //var a = 5;
+            //var b = 3;
+            //var c = 4;
+            //c += +a++ + ++b;
+            //Console.WriteLine(a);
+            //Console.WriteLine(b);
+            //Console.WriteLine(c);
+        }
+    }
+}
