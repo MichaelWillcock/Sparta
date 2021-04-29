b3541814cf4524c71099b7b5d02e39850afe7d1e
Michael Willcock <michaelwillcock7@gmail.com>
2021-04-29 11:00:20 +0100

    Created dnd project file and started to initialise the humanoid class

---
+++ DnDGame/DnDGame/Humanoid.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DnDGame
+{
+    class Humanoid
+    {
+        protected int _hitPoints;
+        protected int _strength;
+        protected int _dexterity;
+        protected int _constitution;
+        protected int _intelligence;
+        protected int _wisdom;
+        protected int _charisma;
+    }
+}
