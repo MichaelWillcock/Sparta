5a0022e26fd5b388284648d4c22637cce21e0353
MichaelWillcock <michaelwillcock7@gmail.com>
2021-06-16 15:07:58 +0100

    Created one customer journey

---
+++ SeleniumPOMWalkthrough/SeleniumPOMWalkthrough/FeatureFolder/AP_SigninJourney.feature
@@ -0,0 +1,11 @@
+﻿Feature: AP_SigninJourney
+	In order to save items to my basket on authenticationpractice.com
+	I need to create an account
+	Given I am on the sign in page I then need to enter a valid email address
+	Once I have entered a valid email address I need to click create account
+	
+
+@mytag
+Scenario: Add two numbers
+	Given that I have navigated from the home page to the sign in page
+	Then I will be able to go to a form to create an account
---
+++ SeleniumPOMWalkthrough/SeleniumPOMWalkthrough/FeatureFolder/AP_SigninJourneySteps.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace SeleniumPOMWalkthrough.FeatureFolder
+{
+    [Binding]
+    public class AP_SigninJourneySteps
+    {
+        AP_SigninSteps steps = new AP_SigninSteps();
+        [Given(@"that I have navigated from the home page to the sign in page")]
+        public void GivenThatIHaveNavigatedFromTheHomePageToTheSignInPage()
+        {
+            steps.GivenIAmOnTheSigninPage();
+        }
+        
+        [Then(@"I will be able to go to a form to create an account")]
+        public void ThenIWillBeAbleToGoToAFormToCreateAnAccount()
+        {
+            steps.GivenIHaveEnteredInTheEmailFieldForCreateAccout("testing160621@snailmail.com");
+            steps.WhenIClickTheCreateAccountButton();
+            steps.ThenAfterWaitingForFiveSecondsIShouldBeTakenToANewPageToCreateAnAccount();
+        }
+    }
+}
